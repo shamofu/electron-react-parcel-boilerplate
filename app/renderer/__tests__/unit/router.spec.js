1ad62e063821eddada24b157996df2675e941090
しゃむ <shamofu@gmail.com>
2017-12-29 23:47:50 +0900

    Add snapshot testing for Router

---
+++ app/renderer/__tests__/unit/router.spec.js
@@ -0,0 +1,23 @@
+/* eslint-env jest */
+
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+
+import Router from '../../router';
+
+test('route /', () => {
+  const component = Renderer.create(
+    <MemoryRouter initialEntries={ ['/'] }><Router /></MemoryRouter>,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('route /start', () => {
+  const component = Renderer.create(
+    <MemoryRouter initialEntries={ ['/start'] }><Router /></MemoryRouter>,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
