91748e43611ac1cd90c800b69fef5078e62c8b33
しゃむ <shamofu@gmail.com>
2017-12-30 20:53:01 +0900

    Minor fix

--- app/main/main.js
+++ app/main/main.js
@@ -23,16 +23,15 @@ app.on('ready', async () => {
   });
 
   const devPath = 'http://localhost:1124';
-
   const prodPath = format({
     pathname: resolve('app/renderer/.parcel/production/index.html'),
     protocol: 'file:',
     slashes: true
   });
-
   const url = isDev ? devPath : prodPath;
-  mainWindow.loadURL(url + '/#/');
+
   mainWindow.setMenu(null);
+  mainWindow.loadURL(url + '/#/');
 });
 
 // Quit the app once all windows are closed
