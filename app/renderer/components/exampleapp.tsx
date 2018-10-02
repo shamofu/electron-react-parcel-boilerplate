cefcc17b7397fb10f3e44c8d466dc216fea6f1f6
shamofu <shamofu@gmail.com>
2018-10-02 09:43:55 +0900

    Update dependencies

--- app/renderer/components/exampleapp.tsx
+++ app/renderer/components/exampleapp.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
-const styles = {
-  App: {
-    marginTop: 100,
-    textAlign: 'center'
-  }
+const styles: React.CSSProperties = {
+  marginTop: 100,
+  textAlign: 'center'
 }
 
 export default () => (
-  <div style={styles.App}>
+  <div style={styles}>
     <h1>Electron + React + Parcel</h1>
     <h3><Link to='/'>Back</Link></h3>
   </div>
