b83a4caa916b1160b4f5c509fbacd7c22d37ce35
shamofu <shamofu@gmail.com>
2019-01-15 16:20:37 +0900

    Watch main process

--- app/main/tests/getapp.ts
+++ app/main/tests/getapp.ts
@@ -3,7 +3,7 @@ import { resolve } from 'app-root-path'
 
 export default () => (
   new Application({
-    path: resolve('./node_modules/.bin/electron' + (process.platform === 'win32' ? '.cmd' : '')),
+    path: resolve('node_modules/.bin/electron' + (process.platform === 'win32' ? '.cmd' : '')),
     args: [resolve('.')],
     startTimeout: 10000,
     waitTimeout: 10000,
