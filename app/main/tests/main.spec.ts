9a021d5b2b70eb37e0e226734a464bd9fc4937fb
shamofu <17104318+shamofu@users.noreply.github.com>
2022-01-16 16:30:51 +0900

    Update dependencies and disable Babel transpilation in Parcel

--- app/main/tests/main.spec.ts
+++ app/main/tests/main.spec.ts
@@ -5,16 +5,3 @@ jest.setTimeout(60000);
 test('Spectron is now deprecated', async () => {
   expect(true).toBe(true);
 });
-
-/*
-test('application title', async () => {
-  expect(await app.client.getTitle()).toBe('Electron-React-Parcel-Boilerplate');
-});
-
-test('go to Start', async () => {
-  const foo = await app.client.$('a#start');
-  await foo.click();
-  const bar = await app.client.$('h1');
-  expect(await bar.getText()).toBe('Electron + React + Parcel');
-});
-*/
